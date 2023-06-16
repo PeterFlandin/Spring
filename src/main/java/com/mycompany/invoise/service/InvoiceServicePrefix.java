eefe0a1928eb4674007ef74863a6e7db622abf97
PeterFlandin <peterflandin@gmail.com>
2023-06-16 15:27:30 +0200

    changement prefix et numerofacture

--- src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -4,10 +4,30 @@ package com.mycompany.invoise.service;
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
 
+import java.io.File;
+import java.util.Date;
+
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface{
 
-    private static long lastNumber=112L;
+    private static long lastNumber;
+    private  String prefix;
+
+    public static long getLastNumber() {
+        return lastNumber;
+    }
+
+    public static void setLastNumber(long lastNumber) {
+        InvoiceServicePrefix.lastNumber = lastNumber;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
     private InvoiceRepositoryInterface invoiceRepository;
 
@@ -20,7 +40,7 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface{
     }
 
     public void createInvoice(Invoice invoice){
-        invoice.setNumber("INV_"+(++lastNumber));
+        invoice.setNumber(prefix +(++lastNumber));
         invoiceRepository.create(invoice);
     }
 }
