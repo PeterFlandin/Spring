6de0723bd43c739d8d83a9747df1ad6a64ed0d7d
PeterFlandin <peterflandin@gmail.com>
2023-06-19 14:43:34 +0200

    ajout appconfig

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -2,7 +2,7 @@ package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -12,7 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
   InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
 
